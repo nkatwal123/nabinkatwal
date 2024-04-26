67c8d0d7516ea07023ba65a52ff986ba18b941e8
nkatwal123 <109146880+nkatwal123@users.noreply.github.com>
2024-04-26 18:43:35 +0545

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "SEO Expert", "Python Developer"],
+        strings: [ "ML Engineer", "Data Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "SEO Expert", "Python Developer"],
+        strings: [ "ML Engineer", "Data Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
